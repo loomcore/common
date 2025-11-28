800f8cfc22ff8dfe192ec25c89b256e724172bba
Phillip Jacobs <phillip.c.jacobs@gmail.com>
2025-11-28 09:47:05 -0600

    Updates

--- src/models/entity.model.ts
+++ src/models/entity.model.ts
@@ -1,5 +1,4 @@
 import { Type } from '@sinclair/typebox';
-import { TypeboxObjectId } from "../validation/index.js";
 
 export interface IEntity {
   _id: string;
@@ -10,7 +9,7 @@ export interface IEntity {
  * Schema definition for the IEntity interface to be used in validation and cleaning
  */
 export const EntitySchema = Type.Object({
-  _id: TypeboxObjectId({ title: 'ID' }),
+  _id: Type.String({ title: 'ID' }),
   _orgId: Type.Optional(Type.String({ title: 'Organization ID' }))
 });
 
