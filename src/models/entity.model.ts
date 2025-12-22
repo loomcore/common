e9cbbbb9f644089895e099c4a648457ec7164f81
Tim Hardy <tim.hardy@omegacreations.com>
2025-12-22 02:41:21 -0600

    Updates

--- src/models/entity.model.ts
+++ src/models/entity.model.ts
@@ -1,16 +1,24 @@
-import { Type } from '@sinclair/typebox';
+import { TSchema, TString, TNumber, Type } from '@sinclair/typebox';
 
-export interface IEntity {
-  // Postgres uses numbers for ids, MongoDB uses strings
-  //  If this becomes problematic, we will need to split the implementation in an intelligent way.
-  _id: string | number;
-  _orgId?: string | number;
+export interface IEntity<TId extends string | number = string> {
+  _id: TId;
+  _orgId?: TId;
 }
 
 /**
  * Schema definition for the IEntity interface to be used in validation and cleaning
  */
-export const EntitySchema = Type.Object({
-  _id: Type.Union([Type.String({ title: 'ID' }), Type.Number({ title: 'ID' })]),
-  _orgId: Type.Optional(Type.Union([Type.String({ title: 'Organization ID' }), Type.Number({ title: 'Organization ID' })]))
-});
+export let EntitySchema: TSchema;
+
+export const createEntitySchema = (idType: TString | TNumber) => {
+  const orgIdType = idType.kind === 'String'
+    ? Type.String({ ...idType, title: 'Organization ID' })
+    : Type.Number({ ...idType, title: 'Organization ID' });
+
+  EntitySchema = Type.Object({
+    _id: idType,
+    _orgId: Type.Optional(orgIdType)
+  });
+
+  return EntitySchema;
+}
