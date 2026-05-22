d23e1d969e770eaf9a7ef86ad4960124bf89fc5c
pcj02b <phillip.c.jacobs@gmail.com>
2026-05-22 13:39:22 -0500

    make updated fields optional

--- src/models/auditable.model.ts
+++ src/models/auditable.model.ts
@@ -5,8 +5,8 @@ import { getIdSchema, TypeboxIsoDate } from '../validation/index.js';
 export interface IAuditable {
   _created: Date;
   _createdBy: AppIdType;
-  _updated: Date;
-  _updatedBy: AppIdType;
+  _updated?: Date;
+  _updatedBy?: AppIdType;
   _deleted?: Date;
   _deletedBy?: AppIdType;
 }
@@ -18,8 +18,8 @@ const idSchema = getIdSchema();
 export const AuditableSchema = Type.Object({
   _created: TypeboxIsoDate(),
   _createdBy: idSchema,
-  _updated: TypeboxIsoDate(),
-  _updatedBy: idSchema,
+  _updated: Type.Optional(TypeboxIsoDate()),
+  _updatedBy: Type.Optional(idSchema),
   _deleted: Type.Optional(TypeboxIsoDate()),
   _deletedBy: Type.Optional(idSchema),
 });
